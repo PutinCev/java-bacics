eafa38fa32b779b2145dfad61bd6583aa90234c8
Путинцев Дмитрий <putdv@mail.ru>
2026-02-09 00:53:07 +0300

    Лабораторная №9 и №10. Refactoring

--- src/org/itmo/lab9/Main.java
+++ src/org/itmo/lab9/Main.java
@@ -4,7 +4,9 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
 
-        //Лаб 9. Задание 2.
+        Scanner scanner = new Scanner(System.in);
+
+//        //Лаб 9. Задание 2.
         List<String> strings = Arrays.asList("apple", "banana", "apple", "orange", "banana");
         Collection<String> uniqueStrings = removeDuplicates(strings);
         System.out.println("Без дубликатов: " + uniqueStrings);
@@ -32,13 +34,15 @@ public class Main {
         System.out.println("LinkedList заполнен за: " + linkedListFillTime + " мс");
 
         System.out.println("\n Произвольный доступ");
-        testRandomAccess(arrayList, linkedList, 100000);
+        System.out.print("Введите количество попыток выбора наугад из заполненного списка: ");
+        int attempts = scanner.nextInt();
+
+        testRandomAccess(arrayList, linkedList, attempts);
 
         //Лаб 9. Задание 4.
         //Так как по условию мы будем использовать класс Пользователь,
         //то при создании объекта пользователь буду создавать новое соответствие имени(строки) и пользователя
         Map<User, Integer> scores = new HashMap<>();
-        Map<String, User> users = new HashMap<>();
 
         User anton = new User("Антон");
         User victor = new User("Виктор");
@@ -46,37 +50,22 @@ public class Main {
         User ivan = new User("Иван");
         User olga = new User("Ольга");
 
-        users.put("Виктор",victor);
-
         scores.put(anton, 2700);
-        users.put("Антон",anton);
-
+        scores.put(victor, 0);
         scores.put(maria, 1350);
-        users.put("Мария",maria);
-
         scores.put(ivan, 2000);
-        users.put("Иван",ivan);
-
         scores.put(olga, 200);
-        users.put("Ольга",olga);
-
-        Scanner scanner = new Scanner(System.in);
 
         System.out.print("Введите имя пользователя: ");
         String userName = scanner.nextLine();
 
-        //найдем пользователя по имени в мапе users
-        User searchUser;
-        if (users.containsKey(userName)) {
+        // Создаем временного пользователя для поиска
+        User searchUser = new User(userName);
+
+        if (scores.containsKey(searchUser)) {
             System.out.println("Пользователь с именем " + userName + " найден");
-            searchUser = users.get(userName);
-            // Ищем очки этого пользователя
-            if (scores.containsKey(searchUser)) {
-                System.out.println("У пользователя " + userName + " " +
-                        scores.get(searchUser) + " очков");
-            } else {
-                System.out.println("У пользователя "+userName+" очки не найдены");
-            }
+            System.out.println("У пользователя " + userName + " " +
+                    scores.get(searchUser) + " очков");
         } else {
             System.out.println("Пользователь не найден");
         }
--- src/org/itmo/lab9/User.java
+++ src/org/itmo/lab9/User.java
@@ -1,4 +1,5 @@
 package org.itmo.lab9;
+import java.util.Objects;
 
 public class User {
     private String name;
@@ -12,4 +13,16 @@ public class User {
     public void setName(String name) {
         this.name = name;
     }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(name, user.name);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
 }
